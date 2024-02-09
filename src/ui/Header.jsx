34cf72d03aae4d749c8de82242d0376f98e7e279
Irina Saveleva <goyalion17@gmail.com>
2024-02-09 22:06:02 +0100

    createBrowserRouter, creating App Layout, fetching data: react router 'loaders', handling errors, fetching orders

---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+
+export default function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <SearchOrder />
+
+      <p>Iryna</p>
+    </header>
+  );
+}
